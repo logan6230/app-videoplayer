d67c517fbef5941391d1326ab3436a0dd2e664cf
Segundo <pantoja2187@gmail.com>
2023-05-02 08:20:58 -0500

    DEV:end:Reproductor de video

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,57 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-videoplayer';
+  title = 'angular-videoplayer-app';
+
+  playlist = [
+    {
+      title: 'Agent 327!',
+      src: 'https://media.vimejs.com/720p.mp4',
+      type: 'video/mp4'
+    },
+    {
+      title: 'Big Buck Bunny',
+      src: 'http://static.videogular.com/assets/videos/big_buck_bunny_720p_h264.mov',
+      type: 'video/mp4'
+    },
+    {
+      title: 'Messi Goal',
+      src: 'http://static.videogular.com/assets/videos/goal-2.mp4',
+      type: 'video/mp4'
+    }
+  ];
+
+  currentIndex = 0;
+  activeVideo = this.playlist[this.currentIndex];
+  api!: { getDefaultMedia: () => { (): any; new(): any; subscriptions: { (): any; new(): any; loadedMetadata: { (): any; new(): any; subscribe: { (arg0: () => void): void; new(): any; }; }; ended: { (): any; new(): any; subscribe: { (arg0: () => void): void; new(): any; }; }; }; }; play: () => void; };
+
+  constructor() {
+  }
+
+  onPlayerSet(api: any) {
+    this.api = api;
+
+    this.api.getDefaultMedia().subscriptions.loadedMetadata.subscribe(this.startVideo.bind(this));
+    this.api.getDefaultMedia().subscriptions.ended.subscribe(this.nextVideo.bind(this));
+  }
+
+  nextVideo() {
+    this.currentIndex++;
+
+    if (this.currentIndex === this.playlist.length) {
+      this.currentIndex = 0;
+    }
+
+    this.activeVideo = this.playlist[this.currentIndex];
+  }
+
+  startVideo() {
+    this.api.play();
+  }
+
+  onClickPlaylistVideo(item: { title: string; src: string; type: string; }, index: number) {
+    this.currentIndex = index;
+    this.activeVideo = item;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import {VgCoreModule} from '@videogular/ngx-videogular/core';
+import {VgControlsModule} from '@videogular/ngx-videogular/controls';
+import {VgOverlayPlayModule} from '@videogular/ngx-videogular/overlay-play';
+import {VgBufferingModule} from '@videogular/ngx-videogular/buffering';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    VgCoreModule,
+    VgControlsModule,
+    VgOverlayPlayModule,
+    VgBufferingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
